dabd993cb2991943b3a0fd62a07ebabf26536de2
CAVillalobos <cvillalobos@nearsoft.com>
2019-03-23 19:57:49 -0700

    mongoDB Atlas established as database

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 module.exports={
     port: process.env.PORT || 3001,
     //Crear base de datos
-    db: process.env.MONGODB||`mongodb://localhost:27017/contacts`
+    db: process.env.MONGODB||`mongodb+srv://testUser:R2uM531suOqsIDTI@cluster0-kvxw6.gcp.mongodb.net/test?retryWrites=true`
 }
