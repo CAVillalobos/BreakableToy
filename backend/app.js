68c7e033125d7ee567308a15a9b0696abb842047
CAVillalobos <cvillalobos@nearsoft.com>
2019-03-24 00:01:35 -0700

    CORS implemented on backend

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,15 @@
-const koa = require("koa");
-const bodyParser = require("koa-bodyparser");
-const koaBody = require('koa-body');
-const app = new koa();
+const koa = require("koa")
+const bodyParser = require("koa-bodyparser")
+const koaBody = require('koa-body')
+const cors = require('koa2-cors')
+const app = new koa()
 const router= require('./routes/index')
-const json = require("koa-json");
+const json = require("koa-json")
 
-app.use(koaBody());
-app.use(bodyParser());
-app.use(json());
+app.use(koaBody())
+app.use(bodyParser())
+app.use(json())
+app.use(cors())
 
 app.use(router.routes())
     .use(router.allowedMethods())
